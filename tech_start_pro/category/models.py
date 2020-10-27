0bc8d4d748c408f914e2c56310ef4f4b4e0b8d10
Matheus Abreu <abreumatheus@icloud.com>
2020-10-27 11:37:57 -0300

    Created category db model

--- tech_start_pro/category/models.py
+++ tech_start_pro/category/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from core.models import AbstractBaseModel
+
+
+class Category(AbstractBaseModel):
+    id = models.IntegerField(primary_key=True, editable=False)
+    name = models.CharField(max_length=255, unique=True, null=False)
+
+    def __str__(self):
+        return self.name
--- tech_start_pro/category/tests.py
+++ tech_start_pro/category/tests.py
@@ -2,9 +2,25 @@ from django.apps import apps
 from django.test import TestCase
 
 from .apps import CategoryConfig
+from .models import Category
 
 
 class TestCategoryAppConfig(TestCase):
     def test_apps(self):
         self.assertEqual(CategoryConfig.name, 'category')
         self.assertEqual(apps.get_app_config('category').name, 'category')
+
+
+class TestCategoryModels(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name='Toys')
+
+    def test_category_model_should_be_instantiated_correctly(self):
+        self.assertIsInstance(self.category, Category)
+        self.assertIsNotNone(self.category.id)
+        self.assertIsNotNone(self.category.created_at)
+        self.assertIsNotNone(self.category.updated_at)
+        self.assertEqual('Toys', self.category.name)
+
+    def test_category_model_should_have_correct_str_representation(self):
+        self.assertEqual('Toys', str(self.category))
