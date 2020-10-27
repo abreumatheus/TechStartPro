7a19c57c55f7d6afb514d220fe16d64f5069df6f
Matheus Abreu <abreumatheus@icloud.com>
2020-10-27 13:21:50 -0300

    Created Product model

--- tech_start_pro/product/models.py
+++ tech_start_pro/product/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from category.models import Category
+from core.models import AbstractBaseModel
+
+
+class Product(AbstractBaseModel):
+    name = models.CharField(max_length=255, null=False)
+    description = models.CharField(max_length=255, null=False)
+    price = models.FloatField(null=False)
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        categories = self.categories.order_by('name')
+        return f'{self.name} - {[category.name for category in categories]}'
--- tech_start_pro/product/tests.py
+++ tech_start_pro/product/tests.py
@@ -2,9 +2,26 @@ from django.apps import apps
 from django.test import TestCase
 
 from product.apps import ProductConfig
+from product.models import Product
 
 
 class TestProductAppConfig(TestCase):
     def test_apps(self):
         self.assertEqual(ProductConfig.name, 'product')
         self.assertEqual(apps.get_app_config('product').name, 'product')
+
+
+class TestProductModels(TestCase):
+    def setUp(self):
+        self.product = Product.objects.create(name='iPhone', price=399.99, description='Test')
+        self.product.categories.create(name='Cell')
+
+    def test_product_model_should_be_instantiated_correctly(self):
+        self.assertIsInstance(self.product, Product)
+        self.assertIsNotNone(self.product.id)
+        self.assertIsNotNone(self.product.created_at)
+        self.assertIsNotNone(self.product.updated_at)
+        self.assertEqual('iPhone', self.product.name)
+
+    def test_product_model_should_have_correct_str_representation(self):
+        self.assertEqual("iPhone - ['Cell']", str(self.product))
