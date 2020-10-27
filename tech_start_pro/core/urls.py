45a8547c7c804493aa0bc72dc1f23790648404f7
Matheus Abreu <abreumatheus@icloud.com>
2020-10-27 14:27:36 -0300

    Implemented drf-yasg for automatic documentation

--- tech_start_pro/core/urls.py
+++ tech_start_pro/core/urls.py
@@ -13,12 +13,29 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from rest_framework.routers import DefaultRouter
 
 from category.routers import main_router as category_router
 from product.routers import main_router as product_router
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="TechStartPro API",
+        default_version='v1',
+        description="An API that provides Products and Categories management.",
+        terms_of_service="#",
+        contact=openapi.Contact(email="abreumatheus@icloud.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 router = DefaultRouter()
 router.registry.extend(category_router.registry)
@@ -27,4 +44,6 @@ router.registry.extend(product_router.registry)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(r'api/', include(router.urls)),
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path(r'', schema_view.with_ui('redoc', cache_timeout=0), name='Documentation'),
 ]
