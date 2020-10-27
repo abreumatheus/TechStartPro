a4ce01bcc31769666113004dc15d4fa9a140f129
Matheus Abreu <abreumatheus@icloud.com>
2020-10-27 11:40:49 -0300

    Added category router to core urls

--- tech_start_pro/core/urls.py
+++ tech_start_pro/core/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from category.routers import main_router as category_router
+
+router = DefaultRouter()
+router.registry.extend(category_router.registry)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(r'api/', include(router.urls)),
 ]
