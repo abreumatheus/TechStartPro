9872bc4ce349b8a6851ce148aaf4a7da27c1124d
Matheus Abreu <abreumatheus@icloud.com>
2020-10-27 12:21:20 -0300

    Added tests for Product app configs

--- tech_start_pro/product/tests.py
+++ tech_start_pro/product/tests.py
@@ -1,3 +1,10 @@
+from django.apps import apps
 from django.test import TestCase
 
-# Create your tests here.
+from product.apps import ProductConfig
+
+
+class TestProductAppConfig(TestCase):
+    def test_apps(self):
+        self.assertEqual(ProductConfig.name, 'product')
+        self.assertEqual(apps.get_app_config('product').name, 'product')
