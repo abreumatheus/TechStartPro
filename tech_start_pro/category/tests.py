9c62daec8931f4da36d9562d13c2d130cda9ccf8
Matheus Abreu <abreumatheus@icloud.com>
2020-10-27 11:35:38 -0300

    Created tests for the category app configs

--- tech_start_pro/category/tests.py
+++ tech_start_pro/category/tests.py
@@ -1,3 +1,10 @@
+from django.apps import apps
 from django.test import TestCase
 
-# Create your tests here.
+from .apps import CategoryConfig
+
+
+class TestCategoryAppConfig(TestCase):
+    def test_apps(self):
+        self.assertEqual(CategoryConfig.name, 'category')
+        self.assertEqual(apps.get_app_config('category').name, 'category')
