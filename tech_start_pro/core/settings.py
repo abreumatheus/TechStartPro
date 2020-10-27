26889b7aef9d6f9f7af3c2b23eff18af83d702fb
Matheus Abreu <abreumatheus@icloud.com>
2020-10-27 12:17:14 -0300

    Added django-filters to settings

--- tech_start_pro/core/settings.py
+++ tech_start_pro/core/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'category',
     'product',
 ]
@@ -71,6 +72,10 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'core.wsgi.application'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
+}
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
